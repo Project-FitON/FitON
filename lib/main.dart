00662aa9dc82ca567d487269642690ae9a8404fb
ihsaan402 <ihsaanar02@gmail.com>
2025-01-30 19:47:48 +0530

    onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
-// Entry point of the Flutter application.
-//Test commoikffglfmo
-//dljfakgjkdagkjlakg
-//jfhggf
-//inrtjddgj join with us never ever find
+
+import 'package:fiton/screens/auth/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const OnboardingScreen(),
+    );
+  }
+}
+
