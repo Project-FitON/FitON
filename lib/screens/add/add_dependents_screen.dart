000d14556f85ea822e2d73851298592a9b51c506
Ranuka Jayesh <118304922+Ranuka-Jayesh@users.noreply.github.com>
2025-03-03 01:10:39 +0530

    3.29 version

---
+++ lib/screens/add/add_dependents_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddDependentsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Dependent'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Text('Add Dependents Screen Placeholder'),
+      ),
+    );
+  }
+}
