7b83c2a805609984eab682c65a6f00f18a52d2a3
gkishalan <princekishalan655@gmail.com>
2025-03-08 14:20:45 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/auth/onboarding_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/add/dependents_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is ready
-
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://oetruvmloogtbbrdjeyc.supabase.co', // Replace with your Supabase URL
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ldHJ1dm1sb29ndGJicmRqZXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5ODkxOTQsImV4cCI6MjA1MzU2NTE5NH0.75FTVi-mQT8JEMIdqNTkN7--Hg1GCuqFydrBnmYzl0o', // Replace with your Supabase anon key
+    url: 'https://oetruvmloogtbbrdjeyc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ldHJ1dm1sb29ndGJicmRqZXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5ODkxOTQsImV4cCI6MjA1MzU2NTE5NH0.75FTVi-mQT8JEMIdqNTkN7--Hg1GCuqFydrBnmYzl0o',
   );
-
-  runApp(FitOn());
+  runApp(const FitOnApp());
 }
 
-class FitOn extends StatelessWidget {
-  const FitOn({super.key});
+class FitOnApp extends StatelessWidget {
+  const FitOnApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FitON',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
       ),
-
       home: Scaffold(
         body: OnboardingScreen(),
         ),
