7dfd913dd97e5c4364bffb4e0d37e85a2b3b89b5
Dilshan Irugalbandara <dilshaniru@email.com>
2025-03-18 19:38:45 +0530

    Reverted Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,3 @@
-import 'package:fiton/screens/cart/cart_screen.dart'; // Importing CartScreen
-import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-void main() async {
-  // Ensure that Flutter binding is initialized before running the app
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Supabase with your Supabase URL and anon key
-  await Supabase.initialize(
-    url: 'https://oetruvmloogtbbrdjeyc.supabase.co', // Your Supabase URL
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ldHJ1dm1sb29ndGJicmRqZXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5ODkxOTQsImV4cCI6MjA1MzU2NTE5NH0.75FTVi-mQT8JEMIdqNTkN7--Hg1GCuqFydrBnmYzl0o', // Your Supabase anon key
-  );
-
-  runApp(const FitOn());
-
 import 'package:fiton/screens/account/notifications_screen.dart';
 import 'package:fiton/screens/add/add_dependents_screen.dart';
 import 'package:fiton/screens/add/dependents_screen.dart';
@@ -23,29 +6,31 @@ import 'package:fiton/screens/fashee/fashee_chat_screen.dart';
 import 'package:fiton/screens/fashee/fashee_screen.dart';
 import 'package:fiton/screens/feed/feed_screen.dart';
 import 'package:flutter/material.dart';
-import 'screens/auth/onboarding_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'screens/add/dependents_screen.dart';
+import 'screens/auth/onboarding_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is ready
+
   await Supabase.initialize(
-    url: 'https://oetruvmloogtbbrdjeyc.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ldHJ1dm1sb29ndGJicmRqZXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5ODkxOTQsImV4cCI6MjA1MzU2NTE5NH0.75FTVi-mQT8JEMIdqNTkN7--Hg1GCuqFydrBnmYzl0o',
+    url: 'https://oetruvmloogtbbrdjeyc.supabase.co', // Replace with your Supabase URL
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ldHJ1dm1sb29ndGJicmRqZXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5ODkxOTQsImV4cCI6MjA1MzU2NTE5NH0.75FTVi-mQT8JEMIdqNTkN7--Hg1GCuqFydrBnmYzl0o', // Replace with your Supabase anon key
   );
-  runApp(const FitOnApp());
+
+  runApp(FitOn());
 }
 
-class FitOnApp extends StatelessWidget {
-  const FitOnApp({super.key});
+class FitOn extends StatelessWidget {
+  const FitOn({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'FitON',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
       ),
-      home: CartScreen(), // Displaying CartScreen as the home screen
       home: Scaffold(
         body: FeedScreen(),
         ),
